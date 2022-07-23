c86113fcca78b7472c87346443c4093e386122c6
Govind Singh <76403417+Govind-singh4822@users.noreply.github.com>
2022-07-23 07:07:29 +0530

    Update Timer.js

--- src/Timer.js
+++ src/Timer.js
@@ -3,36 +3,30 @@ import React, { useState, useRef, useEffect } from 'react'
 
 const Timer = () => {
 
-	// We need ref in this, because we are dealing
-	// with JS setInterval to keep track of it and
-	// stop it when needed
 	const Ref = useRef(null);
 
 	// The state for our timer
-	const [timer, setTimer] = useState('00:00:00');
+	const [timer, setTimer] = useState('00:00');
 
 
 	const getTimeRemaining = (e) => {
 		const total = Date.parse(e) - Date.parse(new Date());
 		const seconds = Math.floor((total / 1000) % 60);
 		const minutes = Math.floor((total / 1000 / 60) % 60);
-		const hours = Math.floor((total / 1000 / 60 / 60) % 24);
+		
 		return {
-			total, hours, minutes, seconds
+			total, minutes, seconds
 		};
 	}
 
 
 	const startTimer = (e) => {
-		let { total, hours, minutes, seconds }
+		let { total,  minutes, seconds }
 					= getTimeRemaining(e);
 		if (total >= 0) {
 
-			// update the timer
-			// check if less than 10 then we need to
-			// add '0' at the beginning of the variable
+			
 			setTimer(
-				(hours > 9 ? hours : '0' + hours) + ':' +
 				(minutes > 9 ? minutes : '0' + minutes) + ':'
 				+ (seconds > 9 ? seconds : '0' + seconds)
 			)
@@ -42,14 +36,10 @@ const Timer = () => {
 
 	const clearTimer = (e) => {
 
-		// If you adjust it you should also need to
-		// adjust the Endtime formula we are about
-		// to code next	
-		setTimer('00:02:00');
+		
+		setTimer('02:00');
 
-		// If you try to remove this line the
-		// updating of timer Variable will be
-		// after 1000ms or 1sec
+	
 		if (Ref.current) clearInterval(Ref.current);
 		const id = setInterval(() => {
 			startTimer(e);
@@ -66,19 +56,13 @@ const Timer = () => {
 		return deadline;
 	}
 
-	// We can use useEffect so that when the component
-	// mount the timer will start as soon as possible
-
-	// We put empty array to act as componentDid
-	// mount only
+	
+	
 	useEffect(() => {
 		clearTimer(getDeadTime());
 	}, []);
 
-	// Another way to call the clearTimer() to start
-	// the countdown is via action event from the
-	// button first we create function to be called
-	// by the button
+	
 	const onClickReset = () => {
 		clearTimer(getDeadTime());
 	}
@@ -92,4 +76,3 @@ const Timer = () => {
 
 export default Timer;
 
-//<button onClick={onClickReset}>Reset</button>
